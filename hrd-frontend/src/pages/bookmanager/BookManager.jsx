80c4e92037f63e2692a6fc4981607f4ec5781d8b
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-12-29 11:49:07 +0900

    edit book

--- hrd-frontend/src/pages/bookmanager/BookManager.jsx
+++ hrd-frontend/src/pages/bookmanager/BookManager.jsx
@@ -5,6 +5,8 @@ import { BookManagerMain } from "./BookManagerMain";
 import { BookManagerViewAdd } from "./BookManagerViewAdd";
 import { BookManagerRent } from "./BookManagerRent";
 import { BookManagerList } from "./BookManagerList";
+import SubHeader from "../../components/SubHeader";
+import { SubNavigation } from "../../components/SubNavigation";
 
 export const BookManager = () => {
     const [items, setItems] = useState([]);
@@ -19,19 +21,19 @@ export const BookManager = () => {
             <SubNavigation
                 basePath="/bookmanager"
                 value={[
-                    { label: "홈으로", path: "" },
                     { label: "회원등록", path: "view-add" },
                     { label: "회원대여 현황", path: "rent" },
-                    { label: "회원목록 조회/수정", path: "list" }
+                    { label: "회원목록 조회/수정", path: "list" },
+                    { label: "홈으로", path: "" }
                 ]}
             />
             {/* 메인내용출력 */}
             <div className="sub-content">
                 <Routes>
-                    <Route path="" element={<BookManagerMain />} />
                     <Route path="view-add" element={<BookManagerViewAdd />} />
                     <Route path="rent" element={<BookManagerRent />} />
                     <Route path="list" element={<BookManagerList />} />
+                    <Route path="" element={<BookManagerMain />} />
                 </Routes>
             </div>
         </>
--- hrd-frontend/src/pages/bookmanager/BookManagerMain.jsx
+++ hrd-frontend/src/pages/bookmanager/BookManagerMain.jsx
@@ -1,6 +1,6 @@
 export const BookManagerMain = () => {
     return (
-        <div>도서대여 관리 프로그램 메인화면입니다.</div>
+        <div>도서대여 관리 프로그램 Home메인화면입니다.</div>
     );
 
 };
